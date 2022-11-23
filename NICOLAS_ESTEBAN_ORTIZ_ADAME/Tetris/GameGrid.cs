2cba072fc0ffcf33ceceaec3d04d6f47fc9b81c8
neortiz1 <neortiz1@espe.edu.ec>
2022-11-22 20:54:12 -0500

    First Commit

---
+++ NICOLAS_ESTEBAN_ORTIZ_ADAME/Tetris/GameGrid.cs
@@ -0,0 +1,79 @@
+﻿namespace Tetris
+{
+    public class GameGrid
+    {
+        
+        //Crear una matriz bidimencional
+        //La primera dimensión es la fila y la segunda dimensi{on es la columna
+        //line1
+        private 
+
+        //Propiedades para la cantidad de filas y columnas
+        //Indexador para facilitar el acceso a la matriz
+        //line2
+        public int 
+        public int 
+
+        //line3 
+        public int this[int r, int c]
+        {
+
+        }
+        //Con está indexación se puede ingresar directamente a un objeto de Gamegrid
+        //El constructor tomará la cantidad de filas y columnas de los parametros. 
+        //Número de columnas
+        //Número de filas
+        //line4
+        public GameGrid(int rows, int columns)
+        {
+
+        }
+        //En el body, se guarda el número de fila, columnas y se inicializa la matriz. 
+        
+        //Metodos convenciales
+        //Verificar si una fila y una columan están dentro de la matriz o no.
+        //line5
+        public bool IsInside(int r, int c)
+        {
+
+        }
+
+        //Verificar si una celda está vacia o no 
+        //Debe estar dentro de la matriz y el valor de entrada en la matriz deber ser cero.
+        //line6
+        public bool IsEmpty(int r, int c)
+        {
+
+        }
+        //Verificar si una fila completa está llena
+        //line7
+        public bool IsRowFull(int r)
+        {
+
+        }
+        //Verificar si una fila está vacia
+        //line8
+        public bool IsRowEmpty(int r)
+        {
+
+        }
+        //Cuando hay filas completas y deben borrarse
+        //line9
+        private void ClearRow(int r)
+        {
+
+        }
+        //Cuando las filas de arriba deben moverse hacia abajo
+        //line10
+        private void MoveRowDown(int r, int numRows)
+        {
+
+        }
+        //Cuando borramos las filas completas
+        //line11
+        public int ClearFullRows()
+        {
+
+        }
+    }
+}
---
+++ NICOLAS_ESTEBAN_ORTIZ_ADAME/Tetris/Position.cs
@@ -0,0 +1,16 @@
+﻿namespace Tetris
+{
+    public class Position
+    {
+        //Para representar un posición o celda en una cuadr{icula
+        //Almacenar una fila y una columna
+        //line1
+        public 
+        public 
+        //Agregar un construcor simple
+        public Position(int row, int column)
+        {
+
+        }
+    }
+}
